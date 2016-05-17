707984eff9d1c7ef4e71ac26a0f1e52a113f110e
akhan23wgu <akhan23@wgu.edu>
2016-05-17 15:05:11 -0600

    TOKEN

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require './app/commands'
 post '/burritoruns' do
   content_type :json
 
-  if params['puBqXcQda8xYA3kvQ80IMMn3'] == ENV['SLACK_VERIFICATION_TOKEN']
+  if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
     team_id = params['team_id']
     channel_id = params['channel_id']
     user_id = params['user_id']
@@ -16,7 +16,7 @@ post '/burritoruns' do
     text = params['text']
     Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
   else
-    {text: "Your mom's a burrito. Try a different command."}.to_json
+    {text: "Nope"}.to_json
   end
 end
 
