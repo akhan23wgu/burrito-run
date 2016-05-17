ef741262c980d11919022cb51858adf2addd77d3
akhan23wgu <akhan23@wgu.edu>
2016-05-17 17:05:16 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,22 +4,21 @@ require 'net/http'
 require './config/i18n'
 require './config/environments'
 require './app/commands'
-require 'pp'
 
 post '/burritoruns' do
   content_type :json
 
- if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
+  if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
     team_id = params['team_id']
     channel_id = params['channel_id']
     user_id = params['user_id']
     user_name = params['user_name']
     text = params['text']
     Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
- else
-	{text: params['token']}.to_json
-	{text: ENV['SLACK_VERIFICATION_TOKEN'].to_json
- end
+  else
+    {text: params['token']}.to_json
+    {text: ENV['SLACK_VERIFICATION_TOKEN']}.to_json
+  end
 end
 
 get '/authed' do
