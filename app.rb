92db9c06912bcf854878bdfc513791c777e0bd81
akhan23wgu <akhan23@wgu.edu>
2016-05-17 16:11:07 -0600

    Added require pp

--- app.rb
+++ app.rb
@@ -4,10 +4,9 @@ require 'net/http'
 require './config/i18n'
 require './config/environments'
 require './app/commands'
-
+require 'pp'
 post '/burritoruns' do
   content_type :json
-
   if params['token'] == ENV['SLACK_VERIFICATION_TOKEN']
     team_id = params['team_id']
     channel_id = params['channel_id']
@@ -16,15 +15,11 @@ post '/burritoruns' do
     text = params['text']
     Commands.execute(team_id, channel_id, user_id, user_name, text).to_json
   else
-    {text: "Your mom's a burrito. Try a different command."}.to_json
+    {text: params['token']}.to_json
   end
 end
-
 get '/authed' do
   uri = URI('https://slack.com/api/oauth.access')
   uri_params = {client_id: ENV['SLACK_CLIENT_ID'], client_secret: ENV['SLACK_CLIENT_SECRET'], code: params['code']}
   uri.query = URI.encode_www_form(uri_params)
   Net::HTTP.get_response(uri)
-
-  redirect 'http://coffee.agelber.com/authed/'
-end
